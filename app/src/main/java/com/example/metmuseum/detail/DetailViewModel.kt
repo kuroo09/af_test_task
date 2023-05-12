9af0f4ad1680b5cf119ebfe261602a2b83460908
kuroo09 <luis.bahr@yahoo.com>
2023-05-12 11:15:24 +0200

    Detail: liveData builder

--- app/src/main/java/com/example/metmuseum/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/metmuseum/detail/DetailViewModel.kt
@@ -1,46 +1,32 @@
 package com.example.metmuseum.detail
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
-import androidx.navigation.NavController
+import androidx.lifecycle.liveData
 import com.example.metmuseum.network.MetApi
-import com.example.metmuseum.network.MetObject
-import com.example.metmuseum.network.MetPhoto
-import kotlinx.coroutines.launch
-import java.lang.Thread.sleep
 
 
 /*
 * TODOs
-* 1-) Convert 3 observation to single one with power of sealed class
-* 2-) use livedata builder
-* 3-) get rid of isLoading as string. Instead, use objects (result of 1. action will cover this one)
-* 4-) Use Ui object instead of directly accessing the api result
+* 1-) Convert 3 observation to single one with power of sealed class -> DONE
+* 2-) use livedata builder -> DONE
+* 3-) get rid of isLoading as string. Instead, use objects (result of 1. action will cover this one) -> DONE
+* 4-) Use Ui object instead of directly accessing the api result -> DONE
+* 5-) Use shared Result class
 *
 * */
 class DetailViewModel(metId: Int) : ViewModel() {
 
-    private val _result = MutableLiveData<Result>()
-    var result: LiveData<Result> = _result
-
-    init {
-        _result.value = Result.Loading
-        getMetObjectById(metId)
-    }
-
     /**
-     * Fetch object information data from the API by passing the tapped ID to the query.
+     * Observed LiveData that handles displaying the data that's fetched from the API.
      */
-    private fun getMetObjectById(metId: Int) {
-        viewModelScope.launch {
-            try {
-                _result.postValue(MetApi.retrofitService.getObjectById(metId).toUiModel())
-            } catch (e: Exception) {
-                _result.value = Result.Error
-            }
+    val result: LiveData<Result> = liveData {
+        emit(Result.Loading)
+        try {
+            emit(MetApi.retrofitService.getObjectById(metId).toUiModel())
+        } catch (e: Exception) {
+            emit(Result.Error)
         }
     }
 }
